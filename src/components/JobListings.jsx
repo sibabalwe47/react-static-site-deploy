00031bf52415331efd428a7fe93e6001080e9bef
Abenzar Sibabalwe Ngqoyiya <abenzarsibabalwe.ngqoyiya@vcontractor.co.za>
2024-08-13 01:25:53 +0200

    modified actual page

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -33,7 +33,7 @@ const JobListings = ({ isHome = false }) => {
     <section className="bg-blue-50 px-4 py-10">
         <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-            {isHome ? 'Recent Jobs' : 'Browse Jobs'}
+            {isHome ? 'Recent Jobs (updated)' : 'Browse Jobs (updated)'}
         </h2>
         
             {loading ? (<Spinner loading={loading} />) : (
